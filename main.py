6f25f17247baa65c6d962fd89d6a7dccd0999075
Adrino Marz <sreesreejume@gmail.com>
2021-07-24 15:48:09 +0530

    new

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print(f"No of minutes in {20} days ")
+calculation_to_units = 24
+name_of_units = "hours"
+
+def days_to_units(num_of_days):
+    print(f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_units}")
+    print("All good!")
+days_to_units(20)
+
